7c9bea5076698b0ce2d4c8b90bb2240d2df84e59
iMac20SteveW <webmaster@wickwoodonline.com>
2014-06-08 23:54:52 -0400

    Added contact text, Denise and Robtert's names, and "THE" to sidebar-header.php Added styles for the additonal header text.

--- trlf/sidebar-header.php
+++ trlf/sidebar-header.php
@@ -8,13 +8,21 @@
 
 <?php if(theme_get_option('theme_header_show_headline')): ?>
 	<?php $headline = theme_get_option('theme_'.(is_home()?'posts':'single').'_headline_tag'); ?>
+	<<?php echo $headline; ?> class="art-headline-one">
+    <a href="<?php echo get_option('home'); ?>/">THE<br /></a>
+</<?php echo $headline; ?>>
 	<<?php echo $headline; ?> class="art-headline">
     <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
 </<?php echo $headline; ?>>
 <?php endif; ?>
 <?php if(theme_get_option('theme_header_show_slogan')): ?>
 	<?php $slogan = theme_get_option('theme_'.(is_home()?'posts':'single').'_slogan_tag'); ?>
-	<<?php echo $slogan; ?> class="art-slogan"><?php bloginfo('description'); ?></<?php echo $slogan; ?>>
+		<<?php echo $slogan; ?> class="art-slogan">
+			<?php bloginfo('description'); ?>
+			<div class="art-header-text">1777 Western Ave. Albany, NY 12203 • Phone: 518-869-1205 • Fax: 518-452-2145</div>
+			<div class="art-header-attorney-left">Denise McCarthy Randall</div>
+			<div class="art-header-attorney-right">Robert G. Randall</div>
+		</<?php echo $slogan; ?>>
 <?php endif; ?>
 
 
@@ -36,6 +44,4 @@
 	get_sidebar('nav'); 
 ?> 
         </div>
-    </nav><?php } ?>
-
-                    
+    </nav><?php } ?>                  
